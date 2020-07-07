bc9b0e6e0234ab3ec8841455e544e31848e992c6
Austin_Keener <austingraphicandweb@gmail.com>
2020-07-07 07:07:40 -0400

    css file path fix and addition of static web page handling in server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3000;
  // 5. sets up the Express app to handle data parsing
  app.use(express.urlencoded({ extended: true }));
  app.use(express.json());
+ app.use(express.static(__dirname + '/public'));
 
  // view route handling -- which works with the html document
  app.get("/", (req,res) => {
