a21d1786a7e22a26eb84f482527730fe856ffbcd
Austin_Keener <austingraphicandweb@gmail.com>
2020-07-07 08:36:14 -0400

    finishing the app.delete function

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const fs = require('fs');
 const express = require("express");
 //  1.1 require path so that i can link documents to each other
 const path = require("path");
+const { response } = require('express');
  // 2. create an instance of express js called app
 const app = express();
  // 3. add a PORT
@@ -19,39 +20,61 @@ const PORT = process.env.PORT || 3000;
  app.use(express.json());
  app.use(express.static(__dirname + '/public'));
 
+ // create get request to get something from server
  // view route handling -- which works with the html document
  app.get("/", (req,res) => {
 //  res.send("Hello world!");
    res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 app.get("/notes", (req,res) => {
-   //  res.send("Hello world!");
       res.sendFile(path.join(__dirname + "/public/notes.html"));
    });
-
+   // this allows the db.json content to post to the webpage in the broswer for the user to see
+app.get("/api/notes",(req, res) => {
+   fs.readFile("./db/db.json",function(err, data){
+      let notes = JSON.parse(data);
+      res.json(notes);
+   })
+})
+// creating a new post request to save to server
 app.post("/api/notes", (req,res) => {
-   // targeting title and text variables
+   // giving every post a unique id
+   var id;
+   // targeting title and text variables to get their values
    let title = req.body.title;
    let text = req.body.text;
-   console.log(title);
-   console.log(text);
    // making json object
    let note = {
+      // id set to null because i do not know the id that will go here yet. it changes for every post.
+      "id":null,
       "title":title,
       "text":text
    }
    // appends to the db.json object 
    let notes;
-   fs.readFile("/db/db.json", function(err, data){
+   // this reads what is in the db.json file
+   fs.readFile("./db/db.json", function(err, data){
          notes = JSON.parse(data);
-         
+         id = notes.length;
+         note.id=id;
+         // notes.push is pushing the inforamtion entered in into the db.json array
+         notes.push(note);
+         //the response is then stringified and the note variable with the new information is returned back to the webpage inside the browser
+      fs.writeFile("./db/db.json", JSON.stringify(notes), err => { 
+         res.json(note);
+      });
    });
-      
-   notes.push(note);
-   fs.writeFile("/db/db.json", JSON.stringify(notes), err => { 
-       console.log("Done writing"); // Success 
-   });
+   //this is going to be the part that deletes the data from the webpage in the browser
+  app.delete("/api/notes/:id", (req, res) => {
+   let id = req.params.id;
+   // this get the 
+   fs.readFile("./db/db.json",function(err, data){
+      let notes = JSON.parse(data);
+      notes.splice(id,1);
+   })
+  })  
 })
+
  // api route handling -- which works with json to get data usually in the form of objects
 
  // 4. listen on the PORT
