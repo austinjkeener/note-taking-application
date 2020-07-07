9753bce3ee0924fca8478da730cb579e507acdeb
Austin_Keener <austingraphicandweb@gmail.com>
2020-07-07 04:48:01 -0400

    added index.html notes.html styles.css

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 
  // 1. require express in the document
 const express = require("express");
+//  1.1 require path so that i can link documents to each other
+const path = require("path");
  // 2. create an instance of express js called app
 const app = express();
  // 3. add a PORT
@@ -15,7 +17,8 @@ const PORT = process.env.PORT || 3000;
 
  // view route handling -- which works with the html document
  app.get("/", (req,res) => {
- res.send("Hello world!");
+//  res.send("Hello world!");
+   res.sendfile(__dirname + "/public/index.html")
 });
  // api route handling -- which works with json to get data usually in the form of objects
 
